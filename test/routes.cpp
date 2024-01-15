13fd7a8fe17c24ef790452e71385282b42e2a992
its-mr-monday <zackary.live8@gmail.com>
2024-01-15 16:16:38 -0500

    moved test route out of header (Oopsie)

--- test/routes.cpp
+++ test/routes.cpp
@@ -1,1 +1,21 @@
 #include "routes.hpp"
+
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
+
+HTTPResponse login(HTTPRequest request) {
+    std::string body = request.body;
+    json j = json::parse(body);
+    std::string username = j["username"];
+    std::string password = j["password"];
+    //Do some db call
+    json response;
+    HTTPResponse res;
+    res.setResponseCode(200);
+    res.setBody(response.dump());
+    res.addHeader("Content-Type", "application/json");
+    res.addHeader("Access-Control-Allow-Origin", "*");
+    res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setVersion("HTTP/1.1");
+    return res;
+}
--- test/routes.hpp
+++ test/routes.hpp
@@ -1,20 +1,3 @@
 #include "restplus.hpp"
-#include "nlohmann/json.hpp"
-using json = nlohmann::json;
 
-HTTPResponse login(HTTPRequest request) {
-    std::string body = request.body;
-    json j = json::parse(body);
-    std::string username = j["username"];
-    std::string password = j["password"];
-    //Do some db call
-    json response;
-    HTTPResponse res;
-    res.setResponseCode(200);
-    res.setBody(response.dump());
-    res.addHeader("Content-Type", "application/json");
-    res.addHeader("Access-Control-Allow-Origin", "*");
-    res.addHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setVersion("HTTP/1.1");
-    return res;
-}
+HTTPResponse login(HTTPRequest);
