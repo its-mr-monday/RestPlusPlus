65988144e79094c09ce8a37f0c376a40dc8852fd
its-mr-monday <zackary.live8@gmail.com>
2024-01-16 11:42:27 -0500

    adding struct for easily used dynamic routing

--- restplus/includes/reststructs.hpp
+++ restplus/includes/reststructs.hpp
@@ -1,30 +1,29 @@
 #include <string>
 #include <map>
 
+struct HTTPRequestParams {
+    std::map<std::string, std::string> params;
+    void add(std::string key, std::string value);
+    void remove(std::string key);
+    std::string get(std::string key);
+    bool isEmpty();
+    bool has(std::string key);
+};
+
 struct HTTPRequest {
     std::string method;
     std::string path;
+    HTTPRequestParams params;
     std::string version;
     std::map<std::string, std::string> headers;
     std::string body;
-    void addHeader(std::string key, std::string value) {
-        headers[key] = value;
-    }
-    void removeHeader(std::string key) {
-        headers.erase(key);
-    }
-    void setBody(std::string body) {
-        this->body = body;
-    }
-    void setMethod(std::string method) {
-        this->method = method;
-    }
-    void setPath(std::string path) {
-        this->path = path;
-    }
-    void setVersion(std::string version) {
-        this->version = version;
-    }
+    void addHeader(std::string key, std::string value);
+    void removeHeader(std::string key);
+    void setBody(std::string body);
+    void setMethod(std::string method);
+    void setPath(std::string path);
+    void setVersion(std::string version);
+    std::string to_string();
 };
 
 struct HTTPResponse {
--- restplus/src/reststructs.cpp
+++ restplus/src/reststructs.cpp
@@ -1,6 +1,29 @@
 #include "reststructs.hpp"
 #include <sstream>
 
+void HTTPRequestParams::add(std::string key, std::string value) {
+    params[key] = value;
+}
+
+void HTTPRequestParams::remove(std::string key) {
+    params.erase(key);
+}
+
+std::string HTTPRequestParams::get(std::string key) {
+    if (params.find(key) == params.end()) {
+        return "";
+    }
+    return params[key];
+}
+
+bool HTTPRequestParams::isEmpty() {
+    return params.empty();
+}
+
+bool HTTPRequestParams::has(std::string key) {
+    return params.find(key) != params.end();
+}
+
 std::string HTTPResponse::to_string() {
     std::stringstream ss;
     ss << this->version << " " << this->response_code << " " << this->status << "\n";
@@ -10,3 +33,58 @@ std::string HTTPResponse::to_string() {
     ss << "\n" << this->body;
     return ss.str();
 }
+
+void HTTPResponse::addHeader(std::string key, std::string value) {
+    headers[key] = value;
+}
+
+void HTTPResponse::removeHeader(std::string key) {
+    headers.erase(key);
+}
+
+void HTTPResponse::setBody(std::string body) {
+    this->body = body;
+}
+
+void HTTPResponse::setResponseCode(int response_code) {
+    this->response_code = response_code;
+}
+
+void HTTPResponse::setVersion(std::string version) {
+    this->version = version;
+}
+
+void HTTPRequest::addHeader(std::string key, std::string value) {
+    headers[key] = value;
+}
+
+void HTTPRequest::removeHeader(std::string key) {
+    headers.erase(key);
+}
+
+void HTTPRequest::setBody(std::string body) {
+    this->body = body;
+}
+
+void HTTPRequest::setMethod(std::string method) {
+    this->method = method;
+}
+
+void HTTPRequest::setPath(std::string path) {
+    this->path = path;
+}
+
+void HTTPRequest::setVersion(std::string version) {
+    this->version = version;
+}
+
+std::string HTTPRequest::to_string() {
+    std::stringstream ss;
+    ss << this->method << " " << this->path << " " << this->version << "\n";
+    for (auto const& x : this->headers) {
+        ss << x.first << ": " << x.second << "\n";
+    }
+    ss << "\n" << this->body;
+    return ss.str();
+}
+
