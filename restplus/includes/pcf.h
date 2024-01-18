fe73fc65658078b06b623855ddee8408a2a15955
its-mr-monday <zackary.live8@gmail.com>
2024-01-18 10:56:34 -0500

    LICENSE update

--- restplus/includes/pcf.h
+++ restplus/includes/pcf.h
@@ -1,3 +1,11 @@
+/*
+    Copyright 2024 by its-mr-monday.
+    All rights reserved
+    This file is part of the RestPlusPlus Framework, and is released 
+    under the "MIT License Agreement". Please see the LICENSE file that 
+    should have been included as part of this package
+*/
+
 #pragma once
 #ifdef __unix__
 #include <unistd.h>
